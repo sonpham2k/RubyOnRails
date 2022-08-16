35d41de82ac5ec4c4ff1e6fd85f5927c519b3305
sonpn-1267 <pham.ngoc.son@sun-asterisk.com>
2022-08-16 13:20:16 +0700

    Chapter 5 Filling_in_the_layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
+  get 'staffs/new'
+  get '/home', to: 'static_pages#home', as:'helf'
+  get '/help', to: 'static_pages#help'
   resources :microposts
   resources :users
   root 'application#hello'
+  get '/signup', to: 'staffs#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
