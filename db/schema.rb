323f537b599ff439d6e5b9ea74764316de6ab3e8
sonpn-1267 <pham.ngoc.son@sun-asterisk.com>
2022-08-18 11:22:43 +0700

    Chapter 9 - Advanced login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_16_073140) do
+ActiveRecord::Schema.define(version: 2022_08_18_031842) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2022_08_16_073140) do
     t.datetime "updated_at", null: false
     t.string "address"
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
